946ed4a64c4d17c10a4a63f8204070631aa781e1
Daniel Lopez <daniellop199731@gmail.com>
2025-09-14 11:40:28 -0500

    Correcciones en arka principal

--- src/main/java/com/bancolombia/arka/ArkaApplication.java
+++ src/main/java/com/bancolombia/arka/ArkaApplication.java
@@ -2,10 +2,8 @@ package com.bancolombia.arka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ArkaApplication {
 
 	public static void main(String[] args) {
